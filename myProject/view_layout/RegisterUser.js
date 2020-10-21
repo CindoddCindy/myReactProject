740d62e5a1f970d1073b53ddc2aff02decdf3a19
CindoddCindy <cindodcindy@gmail.com>
2020-10-21 09:48:19 +0700

    bangkeeee

--- myProject/view_layout/RegisterUser.js
+++ myProject/view_layout/RegisterUser.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 import{
     StyleSheet,
@@ -112,18 +112,162 @@ const RegisterScreen = props => {
                 backgroundColor:'',
                 justifyContent:'',
             }}>
-                <Text style={StyleSheet.successTextStyle}>
+                <Text style={StyleSheet.successTextStyle}>Registrasi Berhasil</Text>
                   <TouchableOpacity
                   style={StyleSheet.buttonStyle}
                   activeOpacity={0.5}
                   onPress={()=> props.navigation.navigate('')} >
-                      
+                    <Text style = {StyleSheet.buttonTextStyle}>Login</Text> 
                   </TouchableOpacity>
-                </Text>
+               
             </View>
-        )
+        );
     }
+    return(
+        <View style={{flex: 1, backgroundColor: ''}}>
+            <Loading loading ={loading}/>
+            <ScrollView keyboardShouldPersistTaps = "handled">
+                <View style ={{alignItems : 'center'}}>
+
+                </View>
+                <KeyboardAvoidingView enabled>
+                    <View style = {StyleSheet.SectionStyle}>
+                        <TextInput
+                        style={StyleSheet.inputStyle}
+                        onChangeText={namaLengkapCustomer=> setNamaLengkapCustomer(namaLengkapCustomer)}
+                        underlineColorAndroid=""
+                        placeholder="Enter Name"
+                        placeholderTextColor=""
+                        autoCapitalize=""
+                        returnKeyType=""
+                        onSubmitEditing={()=>
+                        this.customerEmail && this.customerEmail.focus()
+                        }
+                        blurOnSubmit={false}
+                        />
+                    </View>
+                    <View style ={StyleSheet.SectionStyle}>
+                        <TextInput
+                        style={StyleSheet.inputStyle}
+                        onChangeText={emailCustomer => setEmailCustomer(emailCustomer)}
+                        underlineColorAndroid=""
+                        placeholder=""
+                        placeholderTextColor=""
+                        keyboardType=""
+                        ref={ref =>{
+                            this.customerEmail=ref;
+
+                        }}
+                        returnKeyType=""
+                        onSubmitEditing={()=>this.customerTelpon && this.customerTelpon.focus}
+                        blurOnSubmit={false}
+                        />
+                    </View>
+                    <View style ={StyleSheet.SectionStyle}>
+                        <TextInput
+                        style={StyleSheet.inputStyle}
+                        onChangeText={emailCustomer => setEmailCustomer(emailCustomer)}
+                        underlineColorAndroid=""
+                        placeholder=""
+                        placeholderTextColor=""
+                        keyboardType=""
+                        ref={ref =>{
+                            this.customerEmail=ref;
+
+                        }}
+                        returnKeyType=""
+                        onSubmitEditing={()=>this.customerTelpon && this.customerTelpon.focus}
+                        blurOnSubmit={false}
+                        />
+                    </View>
+                    <View style ={StyleSheet.SectionStyle}>
+                        <TextInput
+                        style={StyleSheet.inputStyle}
+                        onChangeText={emailCustomer => setEmailCustomer(emailCustomer)}
+                        underlineColorAndroid=""
+                        placeholder=""
+                        placeholderTextColor=""
+                        keyboardType=""
+                        ref={ref =>{
+                            this.customerEmail=ref;
+
+                        }}
+                        returnKeyType=""
+                        onSubmitEditing={()=>this.customerTelpon && this.customerTelpon.focus}
+                        blurOnSubmit={false}
+                        />
+                    </View> <View style ={StyleSheet.SectionStyle}>
+                        <TextInput
+                        style={StyleSheet.inputStyle}
+                        onChangeText={emailCustomer => setEmailCustomer(emailCustomer)}
+                        underlineColorAndroid=""
+                        placeholder=""
+                        placeholderTextColor=""
+                        keyboardType=""
+                        ref={ref =>{
+                            this.customerEmail=ref;
+
+                        }}
+                        returnKeyType=""
+                        onSubmitEditing={()=>this.customerTelpon && this.customerTelpon.focus}
+                        blurOnSubmit={false}
+                        />
+                    </View>
+                </KeyboardAvoidingView>
+            </ScrollView>
+        </View>
+    );
 
 
 
-}
+};
+
+export default RegisterUser;
+const styles = StyleSheet.create({
+    SectionStyle: {
+      flexDirection: 'row',
+      height: 40,
+      marginTop: 20,
+      marginLeft: 35,
+      marginRight: 35,
+      margin: 10,
+    },
+    buttonStyle: {
+      backgroundColor: '#7DE24E',
+      borderWidth: 0,
+      color: '#FFFFFF',
+      borderColor: '#7DE24E',
+      height: 40,
+      alignItems: 'center',
+      borderRadius: 30,
+      marginLeft: 35,
+      marginRight: 35,
+      marginTop: 20,
+      marginBottom: 20,
+    },
+    buttonTextStyle: {
+      color: '#FFFFFF',
+      paddingVertical: 10,
+      fontSize: 16,
+    },
+    inputStyle: {
+      flex: 1,
+      color: 'white',
+      paddingLeft: 15,
+      paddingRight: 15,
+      borderWidth: 1,
+      borderRadius: 30,
+      borderColor: 'white',
+    },
+    errorTextStyle: {
+      color: 'red',
+      textAlign: 'center',
+      fontSize: 14,
+    },
+    successTextStyle: {
+      color: 'white',
+      textAlign: 'center',
+      fontSize: 18,
+      padding: 30,
+    },
+  });
